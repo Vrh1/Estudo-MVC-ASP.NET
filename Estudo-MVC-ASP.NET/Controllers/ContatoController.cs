292a78e04361e9b73c229c1a97ec030adce69ad6
Vergara <jvv10.vergara@gmail.com>
2022-10-17 17:36:47 -0300

    Att
    att

--- Estudo-MVC-ASP.NET/Controllers/ContatoController.cs
+++ Estudo-MVC-ASP.NET/Controllers/ContatoController.cs
@@ -1,6 +1,7 @@
 ﻿using Estudo_MVC_ASP.NET.Context;
 using Estudo_MVC_ASP.NET.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
 
 namespace Estudo_MVC_ASP.NET.Controllers
 {
@@ -35,6 +36,61 @@ namespace Estudo_MVC_ASP.NET.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(contato);
+        }
+
+        public IActionResult Editar(int id)
+        {
+            var contato = _context.Contato.Find(id);
+
+            if(contato == null)
+                return NotFound();
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Contato contato)
+        {
+            var contatoBanco = _context.Contato.Find(contato.Id);
+
+            contatoBanco.Name = contato.Name;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Active = contato.Active;
+
+            _context.Contato.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contato.Find(id);
+
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        public IActionResult Deletar(int id)
+        {
+            var contato = _context.Contato.Find(id);
+
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Deletar(Contato contato)
+        {
+            var contatoBanco = _context.Contato.Find(contato.Id);
+
+            _context.Contato.Remove(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
